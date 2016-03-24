0d6642349ba8534310ffd501dc1e28c204daf244
Paul Young <paulyoungonline@gmail.com>
2016-03-24 16:08:09 -0700

    Comment out tests to highlight the issue.

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -1,22 +1,22 @@
 module Test.Main where
 
-import Prelude (Unit(), bind)
+import Prelude (Unit())--, bind)
 
-import Test.ESTree.Expression (checkExpression)
-import Test.ESTree.Identifier (checkIdentifier)
+-- import Test.ESTree.Expression (checkExpression)
+-- import Test.ESTree.Identifier (checkIdentifier)
 import Test.ESTree.Node (checkNode)
-import Test.ESTree.Pattern (checkPattern)
-import Test.ESTree.Position (checkPosition)
-import Test.ESTree.SourceLocation (checkSourceLocation)
+-- import Test.ESTree.Pattern (checkPattern)
+-- import Test.ESTree.Position (checkPosition)
+-- import Test.ESTree.SourceLocation (checkSourceLocation)
 
 import Test.QuickCheck (QC())
 
 
 main :: QC () Unit
 main = do
-  checkPosition
-  checkSourceLocation
-  checkIdentifier
-  checkExpression
-  checkPattern
+  -- checkPosition
+  -- checkSourceLocation
+  -- checkIdentifier
+  -- checkExpression
+  -- checkPattern
   checkNode
